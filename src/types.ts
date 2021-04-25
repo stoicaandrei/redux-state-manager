938b44d5cbcbb387067295c3e974c56c1904ba12
Andrei Stoica <andrei00stoica@gmail.com>
2021-04-25 04:31:08 +0300

    Remove old code

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,6 @@
 export type API<Payload, Result, ApiState> = {
   path: string;
   method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
-  data?: Payload;
-  auth?: boolean;
   successReducer: (state: ApiState, result: Result, payload: Payload) => void;
   failReducer?: (state: ApiState, error: Error, payload: Payload) => void;
   startReducer?: (state: ApiState, payload: Payload) => void;
